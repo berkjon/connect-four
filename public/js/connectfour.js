a58a4532adf252c778537a8a5c00ab9bfe6b70a7
Brian Lin & Jonathan Berk <pair+baffledtree.berkjon@devbootcamp.com>
2015-03-05 15:51:23 -0800

    refactor play turn so it is one function

--- public/js/connectfour.js
+++ public/js/connectfour.js
@@ -8,49 +8,22 @@
   //even: red;
   //odd: black;
 
-
-//board object
-  //contains diskss
-
-//disks object
-  //contains color
-
-//click event for columns
-  // IF class that we click on is a certain column class
-    // IF column has space
-      // placement = HEIGHT - CURRENT_NUMBER_OF_DISKS
-        // HOW TO FIND APPROPRIATE TR
-        // USE nth-child or children()[placement]
-      // PLACE disk AT placement (TR number class)
-        // .css() to change background-color to 'red' if % 0 or 'black' if % 1
-
-
   // add 1 to counter of the click event (global scope)
 
+//Check if column has an unplayed square
+// if true, then run
 
+var play_turn = function() {
+  $('td').on('click', function() {
+    // debugger
+    var current_column = "." + $(this).attr("class")
+    console.log(current_column)
+    $(current_column).find('.unplayed').last().addClass('red played').removeClass('unplayed');
 
-$("table").on('click', '.one', function(e) {
-  console.log("inside");
-  $(this).find(":first-child").addClass("red")
-  // debugger
-  // this.countDiscsInColumn();
-  // this.placeDisc();
-});
+  });
 
-// var countDiscsInColumn = function() {
-//   this.find();
-// }
+}
 
-// var placeDisc = function() {
+play_turn();
 
-// }
-
-
-// var Board = function() {
-//   this.counter = 0;
-//   this.win = false;
-// }
-
-// my_board = new Board();
-// my_board.counter;
 

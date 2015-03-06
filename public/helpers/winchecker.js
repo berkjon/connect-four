f283e1a9c4341674dcd0ca8647c8b8359ee5e0b3
Brian Lin & Cari <pair+baffledtree.cariwest@devbootcamp.com>
2015-03-06 15:57:12 -0800

    refactor horizontal wincon

--- public/helpers/winchecker.js
+++ public/helpers/winchecker.js
@@ -39,6 +39,18 @@ var getSouthWestCell = function(currentDisc) {
   return $(currentDisc).parent().parent().next().find(discClassSouthWest);
 }
 
+var getLeftCell = function(currentDisc) {
+  var currentDiscColumn = getCurrentDiscColumn(currentDisc);
+  var discColumnClassLeft = getColumnClassLeft(currentDiscColumn);
+  return $(currentDisc).parent().parent().find(discColumnClassLeft);
+}
+
+var getRightCell = function(currentDisc) {
+  var currentDiscColumn = getCurrentDiscColumn(currentDisc);
+  var discColumnClassRight = getColumnClassRight(currentDiscColumn);
+  return $(currentDisc).parent().parent().find(discColumnClassRight);
+}
+
 var diagonalCheck = function(currentDisc, color) {
   if (diagonalCheckLeft(currentDisc, color) == 4 || diagonalCheckRight(currentDisc, color) == 4) {
     return true;
@@ -144,3 +156,49 @@ var diagonalSouthWest = function(currentDisc, currentWinCount, currentColor) {
   }
 };
 
+
+var horizontalChecker = function(currentDisc) {
+  var currentWinCount = 1;
+
+  leftWinCount = checkLeft(currentDisc, currentWinCount)
+  totalWinCount = checkRight(currentDisc, leftWinCount)
+
+  if (totalWinCount == 4) {
+    return true;
+  } else {
+    return false;
+  }
+
+};
+
+var checkLeft = function(currentDisc, currentWinCount) {
+  var cellToTheLeft = getLeftCell(currentDisc);
+  var currentPlayerColor = getColorClass(tracker.color);
+  var nextDisc = cellToTheLeft.find(currentPlayerColor);
+
+  if (nextDisc.length === 0) {
+    return currentWinCount;
+  }
+
+  var nextDiscColor = getNextDiscColor(nextDisc);
+  if (currentPlayerColor === nextDiscColor) {
+    currentWinCount++;
+    return checkLeft(nextDisc, currentWinCount);
+  }
+};
+
+var checkRight = function(currentDisc, currentWinCount) {
+  var cellToTheRight = getRightCell(currentDisc);
+  var currentPlayerColor = getColorClass(tracker.color)
+  var nextDisc = cellToTheRight.find(currentPlayerColor);
+
+  if (nextDisc.length === 0) {
+    return currentWinCount;
+  }
+
+  var nextDiscColor = getNextDiscColor(nextDisc);
+  if (currentPlayerColor === nextDiscColor) {
+    currentWinCount++;
+    return checkRight(nextDisc, currentWinCount);
+  }
+};
--- public/js/connectfour.js
+++ public/js/connectfour.js
@@ -20,3 +20,4 @@ $(document).ready(function(){
 
   play_turn();
 });
+

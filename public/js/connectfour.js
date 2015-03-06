1118ead14b3299b08c1c6461fbc497953bf03a3e
Andrew John Schnieder <andrewschnieder@gmail.com>
2015-03-05 17:06:36 -0800

    add helpers js

--- public/js/connectfour.js
+++ public/js/connectfour.js
@@ -13,21 +13,19 @@
 //Check if column has an unplayed square
 // if true, then run
 
-$(document).ready(function){
+$(document).ready(function(){
   tracker = new Tracker
-}
 
-var play_turn = function() {
-  $('td').on('click', function() {
+  var play_turn = function() {
+    $('td').on('click', function() {
     // debugger
-    var current_column = "." + $(this).attr("class")
-    console.log(current_column)
-    $(current_column).find('.unplayed').last().addClass('red played').removeClass('unplayed');
-
-  });
-
-}
+      var current_column = "." + $(this).attr("class")
+      console.log(current_column)
+      $(current_column).find('.unplayed').last().addClass('red played').removeClass('unplayed');
+    });
+  }
 
 play_turn();
 
+});
 

ee4afd82d33271962629acae2bb5599d9a5403ae
Brian Lin & Jonathan Berk <pair+baffledtree.berkjon@devbootcamp.com>
2015-03-05 16:19:08 -0800

    colors change and players can play game

--- public/js/connectfour.js
+++ public/js/connectfour.js
@@ -1,33 +1,32 @@
+$(document).ready(function(){
 
-//Overall put disks into appropriate column
-//column class names: one two three four five six seven
-
-//pseudocode:
-
-//initialize turn counter = 0;
-  //even: red;
-  //odd: black;
-
-  // add 1 to counter of the click event (global scope)
-
-//Check if column has an unplayed square
-// if true, then run
-
-$(document).ready(function){
-  tracker = new Tracker
-}
+  tracker = new Tracker();
 
 var play_turn = function() {
   $('td').on('click', function() {
     // debugger
-    var current_column = "." + $(this).attr("class")
-    console.log(current_column)
-    $(current_column).find('.unplayed').last().addClass('red played').removeClass('unplayed');
+    //if column has unplayed squares, add disc to column
+      //also add to counter for turns
+    if ($(this).find('.unplayed').length) {
+      var current_column = "." + $(this).attr("class")
+      console.log(current_column)
+      $(current_column).find('.unplayed').last().addClass(tracker.color + ' played').removeClass('unplayed');
 
+      console.log(tracker.color);
+      //add to player turn counter
+      tracker.turnColor();
+
+    // } else {
+
+    }
+      //else
+        //call play turn again
   });
 
 }
 
-play_turn();
+//play game function
 
+  play_turn();
 
+});
